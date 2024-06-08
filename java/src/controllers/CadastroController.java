ca42340d12eadc345735c11796ca954dca14b1b1
Andreza Oliveira <andrezacarlos1994@hotmail.com>
2024-06-08 15:54:32 -0300

    remove import spring packages in Cadastro Controller

--- java/src/controllers/CadastroController.java
+++ java/src/controllers/CadastroController.java
@@ -1,12 +1,6 @@
 package controllers;
 import models.*;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 @Controller
 @RequestMapping("/cadastro")
 public class CadastroController {
